a5ddacdaa54b6f55fc2e21a9acdcbeefdfeba2ec
LaaraibAhmed <ahmadlaaraib@gmail.com>
2024-04-02 15:11:56 +0500

    first page done

--- components/userchat.tsx
+++ components/userchat.tsx
@@ -20,7 +20,7 @@ const Userchat = () => {
         <div className="flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
           <p className="text-sm font-normal text-gray-900 dark:text-white">
             {" "}
-            That's awesome. I think our users will really appreciate the
+            That is awesome. I think our users will really appreciate the
             improvements.
           </p>
         </div>
